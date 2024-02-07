3199ee85eac658e082f22944a0dd8cb1ccd62786
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2024-02-07 17:05:39 +0100

    Creating Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />} />
+        <Route index element={<Home />} />
       </Routes>
     </>
   );
---
+++ src/components/Home/index.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div></div>
+    </div>
+  );
+};
+
+export default Home;
