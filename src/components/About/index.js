dba277e6548995b125998bf3877f16155e4c28c9
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2024-02-08 14:56:36 +0100

    about page animation done

---
+++ src/components/About/index.js
@@ -0,0 +1,76 @@
+import {
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faNodeJs,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+  });
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am a full-stack software developer aiming for a master's degree
+            from TU Chemnitz in web engineering.
+          </p>
+          <p>
+            I've been a software developer at Agile Infoways Pvt. Ltd. for more
+            than two years. I obtained a great of practical and professional
+            expertise there.
+          </p>
+          <p>
+            Additionally, I completed an internship at Prakshal IT academy.
+            where I learned the fundamentals of web design.
+          </p>
+        </div>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faNodeJs} color="#68a063" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#f06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28a4d9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#ec4d28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
+};
+
+export default About;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,62 @@
+import "./index.scss";
+import { Link } from "react-router-dom";
+import LogoTitle from "../../assets/images/logo-s.png";
+import { useEffect, useState } from "react";
+import AnimatedLetters from "../AnimatedLetters";
+
 const Home = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const nameArray = ["a", "r", "t", "h"];
+  const jobArray = [
+    "w",
+    "e",
+    "b",
+    " ",
+    "d",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "e",
+    "r",
+  ];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 4000);
+  }, []);
+
   return (
     <div className="container home-page">
-      <div></div>
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2>Fullstack developer / JavaScript Expert</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
     </div>
   );
 };
