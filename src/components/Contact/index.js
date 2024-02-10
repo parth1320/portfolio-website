4ee4d0b4c4815cd58c18cd4190120065126c4d49
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2024-02-10 12:58:45 +0100

    Building Contact Page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,1 +1,33 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+  });
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={["C", "o", "n", "t", "a", "c", "t", " ", "m", "e"]}
+              idx={15}
+            />
+          </h1>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
+};
+
+export default Contact;
