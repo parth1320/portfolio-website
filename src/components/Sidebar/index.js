e7edb4211c9de0094582716fbb35d5473a643a2a
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2024-02-13 14:55:02 +0100

    Try to make resposive

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,57 +3,80 @@ import "./index.scss";
 import Logos from "../../assets/images/logo_par.png";
 import LogoSubtitle from "../../assets/images/logo_p.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faEnvelope,
+  faHome,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { useState } from "react";
 
-const Sidebar = () => (
-  <div className="nav-bar">
-    <Link className="logo" to="/">
-      <img src={Logos} alt="logo" />
-      <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
-    </Link>
-    <nav>
-      <NavLink exact="true" activeClassname="active" to="/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeClassname="active"
-        className="about-link"
-        to="/about"
-      >
-        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeClassname="active"
-        className="contact-link"
-        to="/contact"
-      >
-        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-      </NavLink>
-    </nav>
-    <ul>
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/parth-kakadiya-2a0471140/"
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/" onClick={() => setShowNav(false)}>
+        <img src={Logos} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="parth" />
+      </Link>
+      <nav className={showNav ? "mobile-show" : ""}>
+        <NavLink
+          exact="true"
+          activeClassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}
         >
-          <FontAwesomeIcon icon={faLinkedinIn} color="#4d4d4e" />
-        </a>
-      </li>
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/parth-kakadiya-2a0471140/"
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeClassname="active"
+          className="about-link"
+          to="/about"
+          onClick={() => setShowNav(false)}
         >
-          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-        </a>
-      </li>
-    </ul>
-  </div>
-);
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeClassname="active"
+          className="contact-link"
+          to="/contact"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/parth-kakadiya-2a0471140/"
+          >
+            <FontAwesomeIcon icon={faLinkedinIn} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/parth-kakadiya-2a0471140/"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className="hamburger-icon"
+      />
+    </div>
+  );
+};
 
 export default Sidebar;
